9abc536ad4d0ac13ec29b587f8281bd47c101e3c
Marlon Jimenez <marlonji2360@gmail.com>
2025-06-11 13:47:06 -0600

    Configuración e ingreso de la clase Conexión a la base de datos

---
+++ ReportesRegulatorios/Modelos/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportesRegulatorios.Modelos
+{
+    internal class Conexion
+    {
+        private SqlConnection conexion;
+
+        public Conexion()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            conexion = new SqlConnection(connectionString);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
