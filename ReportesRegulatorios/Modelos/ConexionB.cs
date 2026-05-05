bf74d33308812358b634a349dc962cb31369a5a3
Marlon Jimenez <marlonji2360@gmail.com>
2026-05-05 08:11:50 -0600

    Creación de modulos

---
+++ ReportesRegulatorios/Modelos/ConexionB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportesRegulatorios.Modelos
+{
+    internal class ConexionB
+    {
+        private SqlConnection conexion;
+
+        public ConexionB()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConexionSQL"].ConnectionString;
+            conexion = new SqlConnection(connectionString);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
